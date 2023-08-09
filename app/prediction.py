e2929aabf0cc4a826e418301e8ab9df9e80d89cd
Moubashsir Jawed <jawedmoubashsir@gmail.com>
2023-08-09 18:41:30 +0000

    added prediction framework for flaskapp

---
+++ app/prediction.py
@@ -0,0 +1,24 @@
+
+#import requests
+import web_service
+
+bike_data = {
+    'season': 1,
+    'mnth': 1,
+    'holiday': 0,
+    'weekday': 6, 
+    'workingday': 0, 
+    'weathersit': 2,
+    'temp': 0.344167, 
+    'atemp': 0.363625,
+    'hum': 0.805833,
+    'windspeed': 0.160446
+}
+
+#url = 'http://localhost:9696/predict'
+#response = requests.post(url, json=bike_data)
+#print(response.json())
+
+pred = web_service.predict(bike_data)
+print(pred)
+
---
+++ app/web_service.py
@@ -0,0 +1,51 @@
+
+
+import os
+
+import mlflow
+from flask import Flask, request, jsonify
+
+from google.cloud import storage
+client = storage.Client()
+
+TRACKING_SERVER_HOST = "34.125.75.97"
+TRACKING_SERVER_PORT = "5000"
+MODEL_NAME = "random-forest-regressor"
+TRACKING_URI=f"http://{TRACKING_SERVER_HOST}:{TRACKING_SERVER_PORT}"
+
+def load_model_from_registry():
+    """
+    Loads the ML model from the MLFlow registry
+    """
+    tracking_uri = TRACKING_URI
+    mlflow.set_tracking_uri(tracking_uri)
+    model_uri = f"models:/{MODEL_NAME}/latest"
+    loaded_model = mlflow.pyfunc.load_model(model_uri)
+    print("Model loaded from registry")
+
+    return loaded_model
+
+def predict(features):
+    model = load_model_from_registry()
+    preds = model.predict(features)
+    return float(preds[0])
+
+
+#app = Flask('ride-prediction')
+
+#@app.route('/predict', methods=['POST'])
+def predict_endpoint():
+    bike_data = request.get_json()
+
+    pred = predict(bike_data)
+
+    result = {
+        'count': pred,
+        #'model_version': load_model_from_registry
+    }
+
+    return jsonify(result)
+
+#if __name__ == "__main__":
+#    app.run(debug=True, host='0.0.0.0', port=9696)
+
